8e1b27493d1f556c5a2330f85f6a03690c6e4395
MichalisGianniotis <mixalismixalis001@gmail.com>
2025-11-27 15:56:35 +0200

    update

--- script.js
+++ script.js
@@ -1,48 +1,13 @@
-// function displayresult() {
-//             let res = document.getElementById("result");
-//             res.style.display = "block";
-//         }
-function countOnes(cash) {
-    return cash * 1;
-}
-function countTwos(cash) {
-    return cash * 2;
-}
-function countFives(cash) {
-    return cash * 5;
-}
-function countTens(cash) {
-    return cash * 10;
-}
-function countTwenties(cash) {
-    return cash * 20;
-}
-function countFifties(cash) {
-    return cash * 50;
-}
-function countHundreds(cash) {
-    return cash * 100;
-}
-function countTwoHundreds(cash) {
-    return cash * 200;
-}
 function sumOfCash() {
-    let ones = document.getElementById("i1").value;
-    let twos = document.getElementById("i2").value;
-    let fives = document.getElementById("i3").value;
-    let tens = document.getElementById("i4").value;
-    let twenties = document.getElementById("i5").value;
-    let fifties = document.getElementById("i6").value;
-    let hundreds = document.getElementById("i7").value;
-    let twoHundreds = document.getElementById("i8").value;
-    ones = countOnes(ones);
-    twos = countTwos(twos);
-    fives = countFives(fives);
-    tens = countTens(tens);
-    twenties = countTwenties(twenties);
-    fifties = countFifties(fifties);
-    hundreds = countHundreds(hundreds);
-    twoHundreds = countTwoHundreds(twoHundreds);
+    let ones = document.getElementById("i1").value * 1;
+    let twos = document.getElementById("i2").value * 2;
+    let fives = document.getElementById("i3").value * 5;
+    let tens = document.getElementById("i4").value * 10;
+    let twenties = document.getElementById("i5").value * 20;
+    let fifties = document.getElementById("i6").value * 50;
+    let hundreds = document.getElementById("i7").value * 100;
+    let twoHundreds = document.getElementById("i8").value * 200;
+
     let sum = ones + twos + fives + tens + twenties + fifties + hundreds + twoHundreds;
     let result = document.getElementById("result");
     result.textContent =  "Έχεις : " + sum + " €";
